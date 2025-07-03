bf66cf4a0de290976f2288eaa99c0a8636574692
Umer-Jahangir <umerjahangir.cs@gmail.com>
2025-07-04 00:19:47 +0500

    fix: remove standard libs from py2app package list

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ APP = ['instagram_scraper.py']
 DATA_FILES = []
 OPTIONS = {
     'argv_emulation': True,
-    'packages': ['selenium', 'getpass', 're', 'csv', 'time', 'random', 'platform'],
-    # 'iconfile': 'icon.icns', 
+    'packages': ['selenium'],  # ONLY third-party packages
+    # 'iconfile': 'icon.icns',  # optional icon
 }
 
 setup(
